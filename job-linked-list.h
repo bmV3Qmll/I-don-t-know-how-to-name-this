cee18da62c0b3faf2adf004e8d9785ab01e94ac9
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-09 22:48:13 +0700

    Create job-linked-list.h
    error-prone header file

---
+++ job-linked-list.h
@@ -0,0 +1,8 @@
+void * Malloc(size_t)
+struct job;
+job * construct(pid_t, int, job*, char*)
+struct linked-list;
+linked-list jobs;
+job * push(pid_t, char *)
+void erase(pid_t)
+void print-jobs(int)
