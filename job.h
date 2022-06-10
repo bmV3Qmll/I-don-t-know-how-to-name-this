bf77f080c948e022327d92287a6f21e42e37dc99
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-10 10:40:55 +0700

    Update and rename job-linked-list.h to job.h

--- job.h
+++ job.h
@@ -2,9 +2,19 @@
 #define JOB_H_
 
 void * Malloc(size_t);
-struct job;
+typedef struct job{
+    pid_t pid;
+    int jid;
+    job * next;
+    char * desc;
+} job;
+
+struct linked_list{
+    job * head;
+    job * tail;
+} * jobs;
+
 job * construct(pid_t, int, job*, char*);
-struct linked_list;
 job * push(pid_t, char *);
 void erase(pid_t);
 void print_jobs(int);
