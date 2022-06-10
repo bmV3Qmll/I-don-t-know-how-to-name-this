ffc1d0ac5781372c5f0b4577d35425172e720883
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-10 08:25:31 +0700

    Update job-linked-list.c
    fix bugs and test code

--- job-linked-list.c
+++ job-linked-list.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifndef MAX_LEN
-#define MAX_LEN 128
-#endif
+typedef struct job job;
 
 void * Malloc(size_t size){
   void * res = malloc(size);
@@ -13,53 +11,59 @@ void * Malloc(size_t size){
 }
 
 struct job{
-  pid_t pid;
+  int pid;
   int jid;
   job * next;
-  char * desc[MAX_LEN];
+  char * desc;
 };
 
-job * construct(pid_t p = 0, int j = 0, job * prev = NULL, char * des = NULL){
+job * construct(int p, int j, job * prev, char * des){
   job * t = Malloc(sizeof(job));
   t->pid = p;
   t->jid = j;
-  prev->next = t;
-  t->desc = dec;
+  if (prev){prev->next = t;} // jobs->head don't have prev pointer
+  t->desc = des;
 }
 
-// linked-list contains two empty end nodes head && tail; count to keep track of the number of jobs
-struct linked-list{
-  int count = 0;
-  job * head = construct();
-  job * tail = construct(0, 0, head);
-};
+// linked_list contains two empty end nodes head && tail; count to keep track of the number of jobs
+struct linked_list{
+  int count;
+  job * head;
+  job * tail;
+} * jobs;
+/*
+	in main implementation, declare jobs as follow:
+	jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
+  	jobs->count = 0;
+  	jobs->head = construct(2367, 10, NULL, NULL);
+  	jobs->tail = construct(2879, 11, jobs->head, NULL);
+*/
 
-linked-list jobs{};
-
-job * push(pid_t p, char * dec){ // O(1)
-  job * newTail = construct();
-  jobs.tail->pid = p;
-  jobs.tail->jid = ++jobs.count;
-  jobs.tail->next = newTail;
-  jobs.tail->desc = dec;
-  jobs.tail = newTail;
+job * push(int p, char * des){ // O(1)
+  job * newTail = construct(0, 0, NULL, NULL);
+  jobs->tail->pid = p;
+  jobs->tail->jid = ++jobs->count;
+  jobs->tail->next = newTail;
+  jobs->tail->desc = des;
+  jobs->tail = newTail;
 }
 
-void erase(pid_t p){ // O(n)
-  if (!jobs.count){return;}
-  job * iter = jobs.head;
-  while (; iter != jobs.tail; iter = iter->next){
+void erase(int p){ // O(n)
+  if (!jobs->count){return;}
+  job * iter = jobs->head;
+  for (; iter != jobs->tail; iter = iter->next){
     if (iter->next->pid == p){
       iter->next = iter->next->next;
       iter = iter->next;
-      --jobs.count;
+      --jobs->count;
       break;
     }
   }
-  while (; iter != jobs.tail; iter = iter->next){
+  for (; iter != jobs->tail; iter = iter->next){
     --iter->jid;
   }
 }
+
 
 void print-jobs(int fd){ // O(n)
   job * iter = jobs.head->next;
@@ -67,3 +71,19 @@ void print-jobs(int fd){ // O(n)
     dprintf(fd, "[%d] %d %s &\n", iter->jid, iter->pid, iter->desc);
   }
 }
+/*
+debugging code
+
+int main(){
+  jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
+  jobs->count = 0;
+  jobs->head = construct(2367, 10, NULL, NULL);
+  jobs->tail = construct(2879, 11, jobs->head, NULL);
+  push(1189, "cat hello.txt | sort");
+  job * sec = push(2205, "ls -la");
+  push(3456, "strace test.c");
+  erase(1189);
+  print_jobs();
+  return 0;
+}
+*/
