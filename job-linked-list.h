0a13a25eb230ec97025dfe40adbe4a8220b8eddf
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-10 08:43:20 +0700

    Update job-linked-list.h

--- job-linked-list.h
+++ job-linked-list.h
@@ -1,8 +1,12 @@
-void * Malloc(size_t)
+#ifndef JOB_H_
+#define JOB_H_
+
+void * Malloc(size_t);
 struct job;
-job * construct(pid_t, int, job*, char*)
-struct linked-list;
-linked-list jobs;
-job * push(pid_t, char *)
-void erase(pid_t)
-void print-jobs(int)
+job * construct(pid_t, int, job*, char*);
+struct linked_list;
+job * push(pid_t, char *);
+void erase(pid_t);
+void print_jobs(int);
+
+#endif
