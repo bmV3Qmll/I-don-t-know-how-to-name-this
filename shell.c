1961d3d8c4144f0ae538846c646be93f62010b25
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-13 11:29:17 +0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -44,6 +44,15 @@ int Kill(pid_t pid, int sig){
     return 0;
 }
 
+int Sigprocmask(int how, const sigset_t *restrict set, sigset_t *restrict oldset){
+    int ret = sigprocmask(how, set, oldset);
+    if (ret < 0){
+        perror("sigprocmask error");
+        exit(1);
+    }
+    return ret;
+}
+
 /*  This function parses arguemnts to argv and returns whether user want to run in background or not
     buf is a modifiable copy of input command 
 */
@@ -80,37 +89,36 @@ int built_in(char * argv[]){
     if (*argv[1] == '%'){j = atoi(argv[1] + 1);}
     else{p = atoi(argv[1]);}
 
-    if (!j && !p){
+    if (!j || !p){
         perror("Invalid ID");
         return 0;
     }
     
-    int fg = (argv[0] == "fg");
-    // if process stopped, send SIGCONT
+    int fg = !strncmp(argv[0], (char *) "fg", 2);
+    int stat;
     p = waitpid(p, &stat, WNOHANG);
     if (p && WIFSTOPPED(stat)){
+        // if process stopped, send SIGCONT
         Kill(p, SIGCONT);
         if (fg){waitpid(p, &stat, 0);}
     }else if (p){
         //  process terminated but hadn't been erased
         //  fork new one
         job * tar = search(j, p)->next;
-        if (!tar){return 1;}
+        if (!tar){return 1;}    // 404
         char * des = tar->desc;
-        if (fg){}
-        eval();
+        if (fg){ // des ends with '   &   \n' -> fix this
+            char * temp = strchr(des, '&');
+            *temp = '\0';
+        }
+        eval(des);
         //  old process will be deleted by sigchild_handler
     }// else p still runs
-    if {
-        
-    }else{
-        
-    }
     return 1;
 }
 
 
-// These are signal handler for SIGINT, SIGCHILD and SIGSTP
+// These are signal handlers for SIGINT, SIGCHILD and SIGSTP
 void sigint_handler(int sig){
     Kill(fg_pid, SIGINT); // Sends SIGINT to foreground process
     return;
@@ -118,10 +126,20 @@ void sigint_handler(int sig){
 
 void sigchild_handler(int sig){
     pid_t child_pid;
+    int old_errno = errno;  // save errno so that following function don't interfere other parts that rely on errno
+    sigset_t full_mask, prev_mask;
+    sigfillset(&full_mask);
     while((child_pid = waitpid(-1, NULL, 0)) > 0){
+        // Protect accesses to shared global data structure (jobs) by blocking all signals
+        Sigprocmask(SIG_BLOCK, &full_mask, &prev_mask);
         erase(search(-1, child_pid));
         // log something maybe
+        Sigprocmask(SIG_SETMASK, &prev_mask, NULL);
     }
+    if (errno != ECHILD){
+        perror("waitpid error");
+    }
+    errno = old_errno;  // restore errno
 }
 
 void sigstp_handler(int sig){
@@ -136,34 +154,56 @@ void eval(char * cmd){
     int bg;
     pid_t pid;
     
+    // Initialize masks
+    sigset_t full_mask, child_mask, prev_child;
+    sigfillset(&full_mask);
+    sigemptyset(&child_mask);
+    sigaddset(&child_mask, SIGCHILD);
+    
+    // Register signal handler
+    Signal(SIGINT, sigint_handler); 
+    Signal(SIGCHILD, sigchild_handler);
+    Signal(SIGSTP, sigstp_handler);
+    
     strncpy(buf, cmd, strlen(cmd));
     bg = parseline(buf, argv);
     
     if (argv[0] == NULL){return;} // Empty command line
     
     if (!built_in(argv)){
+/* Mitigate race condition
+If the child runs in background, it must be added to job lists, and when it terminated, erase will be called by sigchild_handler.
+However, the following sequences can lead to error:
+1. Child terminates before context switches to parent, in which SIGCHILD is sent to parent
+2. sigchild_handler is triggered, and call erase, which deletes the non-existence job
+3. After that, parent call push to add the terminated job to lists
+
+To prevent this, the parent must pre-block SIGCHILD until push is called
+-> Synchronizing Flows to Avoid Concurrency Bugs
+*/
         // fork and get child process execve the executable file
+        Sigprocmask(SIG_BLOCK, &child_mask, &prev_child); // block SIGCHILD
         if ((pid = Fork()) == 0){
+            Sigprocmask(SIG_SETMASK, &prev_child, NULL); // child process -> unblock SIGCHILD 
             if (execve(argv[0], argv, environ) < 0){
                 perror("execve error");
                 exit(1);
             }
         }
-        // Register signal handler
-        Signal(SIGINT, sigint_handler); 
-        Signal(SIGCHILD, sigchild_handler);
-        Signal(SIGSTP, sigstp_handler);
         
         if (bg){
             printf("%d: %s", pid, cmd);
+            // Protect accesses to shared global data structure (jobs) by blocking all signals
+            Sigprocmask(SIG_BLOCK, &full_mask, NULL);
             push(pid, cmd);
         }else{
             fg_pid = pid;
             int stat;
             if (waitpid(pid, &stat, 0) < 0){
-                perror("wait error");
+                perror("waitpid error");
             }
         }
+        Sigprocmask(SIG_SETMASK, &prev_child, NULL); // reset signal mask
     }
     return;
 }
