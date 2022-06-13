c64b79d12139b0c3b8ebe8e28fa079ec2baa5ad6
bmV3Qmll <xthanh051423@gmail.com>
2022-06-13 17:13:18 +0700

    debugging job.c

--- job.c
+++ job.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 typedef struct job job;
+typedef struct linked_list ll;
 
 void * Malloc(size_t size){
     void * res = malloc(size);
@@ -23,6 +24,7 @@ job * construct(pid_t p, int j, job * prev, char * des){
     t->jid = j;
     if (prev){prev->next = t;} // jobs->head don't have prev pointer
     t->desc = des;
+    return t;
 }
 
 volatile int count = 0;     // tell the compiler not to cache count, which may be updated by signal handler
@@ -31,25 +33,24 @@ volatile int count = 0;     // tell the compiler not to cache count, which may b
 struct linked_list{
     job * head;
     job * tail;
-} * jobs;
+};
 /*
     in main implementation, declare jobs as follow:
-    jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
+    ll * jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
     jobs->head = construct(0, 0, NULL, NULL);
     jobs->tail = construct(0, 0, jobs->head, NULL);
 */
 
-job * push(pid_t p, char * des){ // O(1)
-    job * newTail = construct(0, 0, NULL, NULL);
+void push(ll * jobs, pid_t p, char * des){ // O(1)
+    job * newTail = construct(0, 0, jobs->tail, NULL);
     jobs->tail->pid = p;
     jobs->tail->jid = ++count;
-    jobs->tail->next = newTail;
     jobs->tail->desc = des;
     jobs->tail = newTail;
 }
 
 //  Note: returns the previous pointer to target
-job * search(int j, pid_t p){ // for unknown variables, set to -1
+job * search(ll * jobs, int j, pid_t p){ // for unknown variables, set to -1
     job * iter = jobs->head;
     for (; iter != jobs->tail; iter = iter->next){
         if (iter->next->jid == j || iter->next->pid == p){return iter;}
@@ -69,31 +70,48 @@ void print_job(job * it, int fd){
     dprintf(fd, "[%d] %d %s &\n", it->jid, it->pid, it->desc);
 }
 
-void list_jobs(int fd){ // O(n)
+void list_jobs(ll * jobs, int fd){ // O(n)
     job * iter = jobs->head->next;
     for (; iter != jobs->tail; iter = iter->next){
         print_job(iter, fd);
     }
     dprintf(fd, "----------------\n");
 }
+
+void free_all(ll * jobs){
+    job * iter = jobs->head;
+    while (iter->next != jobs->tail){
+    	erase(iter);
+    }
+}
 /*
 debugging code
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include "job.h"
+
+extern volatile int count;
 
 int main(){
-    jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
+    printf("%d\n", count);
+    struct linked_list * jobs = (struct linked_list *) Malloc(sizeof(struct linked_list));
     jobs->head = construct(0, 0, NULL, NULL);
     jobs->tail = construct(0, 0, jobs->head, NULL);
-    push(1189, "cat hello.txt | sort");
-    job * sec = push(2205, "ls -la");
-    push(3456, "strace test.c");
-    erase(search(-1, 2789));
-    list_jobs(1);
-    erase(search(-1, 2205));
-    list_jobs(1);
-    job * t = search(-1, 3456)->next;
-    job * f = search(1, -1)->next;
+    push(jobs, 1189, "cat hello.txt | sort");
+    push(jobs, 2205, "ls -la");
+    push(jobs, 3456, "strace test.c");
+    erase(search(jobs, -1, 2789));
+    list_jobs(jobs, 1);
+    erase(search(jobs, -1, 2205));
+    list_jobs(jobs, 1);
+    job * t = search(jobs, -1, 3456)->next;
+    job * f = search(jobs, 1, -1)->next;
     print_job(t, 1);
     print_job(f, 1);
     return 0;
 }
+
 */
--- job.h
+++ job.h
@@ -2,7 +2,7 @@
 #define JOB_H_
 
 typedef struct job job;
-
+typedef struct linked_list ll;
 void * Malloc(size_t);
 struct job{
     pid_t pid;
@@ -11,18 +11,19 @@ struct job{
     char * desc;
 };
 
-volatile int count;
+//volatile int count;
 
 struct linked_list{
     job * head;
     job * tail;
-} * jobs;
+};
 
 job * construct(pid_t, int, job*, char*);
-job * push(pid_t, char *);
-job * search(int, pid_t);
+void push(ll *, pid_t, char *);
+job * search(ll *, int, pid_t);
 void erase(job *);
 void print_job(job *, int);
-void list_jobs(int);
+void list_jobs(ll *, int);
+void free_all(ll *);
 
 #endif
