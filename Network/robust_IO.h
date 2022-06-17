3ce10f8fa60f253c451e0f6c3cef881b70802aca
bmV3Qmll <104069545+bmV3Qmll@users.noreply.github.com>
2022-06-17 10:25:17 +0700

    Create robust_IO.h

---
+++ Network/robust_IO.h
@@ -0,0 +1,20 @@
+#ifndef ROBUST_IO_H_
+#define ROBUST_IO_H_
+
+typedef struct rio rio
+ssize_t readn(int, const void *, size_t);
+ssize_t writen(int, const void *, size_t);
+
+struct rio{
+	int fd;					// file descriptor
+	int cnt;				// number of unread bytes in buf
+	char * ptr;				// pointer to next unread byte
+	char buf[BUF_SIZE];		// internal buffer
+};
+
+void rio_init(rio *, int);
+ssize_t buf_read(rio *, const void *, size_t);
+ssize_t buf_readline(rio *, const void *, size_t);
+ssize_t buf_readn(rio *, const void *, size_t);
+
+#endif
